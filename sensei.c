1737ba1c1e8281dd68c88081b91629b2b0dee062
giganticbite <matcake.1616@gmail.com>
2020-04-25 22:46:04 +0900

    implemented calculator

--- sensei.c
+++ sensei.c
@@ -47,6 +47,10 @@ char *user_input;
 // token
 Token *token;
 
+Node *primary();
+Node *mul();
+Node *expr();
+
 void error(char *fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
@@ -107,8 +111,7 @@ Token *new_token(TokenKind kind, Token *cur, char *str){
 
 // tokenize p
 // space -> +,- -> number
-Token *tokenize(){
-  char *p = user_input;
+Token *tokenize(char *p){
   Token head;
   head.next = NULL;
   Token *cur = &head;
@@ -119,7 +122,8 @@ Token *tokenize(){
       p++;
       continue;
     }
-    if(*p == '+' || *p == '-'){
+    if(*p == '+' || *p == '-' || *p == '*' 
+    || *p == '/' || *p == '(' || *p == ')'){
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
@@ -159,24 +163,6 @@ Node *new_node_num(int val){
 // primary ::= num | "(" expr ")"
 // implemented by recursive descent parsing (LL(1) parser)
 
-Node *expr(){
-  Node *node = mul();
-  while(1){
-    if(consume('+')) node = new_node(ND_ADD, node, mul());
-    else if(consume('-')) node = new_node(ND_SUB, node, mul());
-    else return node;
-  }
-}
-
-Node *mul(){
-  Node *node = primary();
-  while(1){
-    if(consume('*')) node = new_node(ND_MUL, node, primary());
-    else if(consume('/')) node = new_node(ND_DIV, node, primary());
-    else return node;
-  }
-}
-
 Node *primary(){
   // if next token is "(", primary must be going to be "(" expr ")"
   if(consume('(')){
@@ -189,36 +175,57 @@ Node *primary(){
   return new_node_num(expect_number());
 }
 
+Node *mul(){
+  Node *node = primary();
+  while(1){
+    if(consume('*')) node = new_node(ND_MUL, node, primary());
+    else if(consume('/')) node = new_node(ND_DIV, node, primary());
+    else return node;
+  }
+}
+
+Node *expr(){
+  Node *node = mul();
+  while(1){
+    if(consume('+')) node = new_node(ND_ADD, node, mul());
+    else if(consume('-')) node = new_node(ND_SUB, node, mul());
+    else return node;
+  }
+}
+
+// generate codes by node
 void gen(Node *node){
   if(node->kind==ND_NUM){
-    printf("   push %d\n");
+    printf("  push %d\n", node->val);
     return;
   }
 
   gen(node->lhs);
   gen(node->rhs);
 
-  printf("   pop rdi\n");
-  printf("   pop rax\n");
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
 
   switch(node->kind){
     case ND_ADD:
-    printf("   add rax, rdi\n");
+    printf("  add rax, rdi\n");
     break;
     case ND_SUB:
-    printf("   aub rax, rdi\n");
+    printf("  sub rax, rdi\n");
     break;
     case ND_MUL:
-    printf("   imul rax, rdi\n");
+    printf("  imul rax, rdi\n");
     break;
     case ND_DIV:
-    printf("   cqo\n"); // extend rax(64bit to 128bit) 
-    printf("   idiv rdi\n"); // take div of rax and rdx(quotient->rax, remainder->rdx)
+    printf("  cqo\n"); // extend rax(64bit to 128bit) 
+    printf("  idiv rdi\n"); // take div of rax and rdx(quotient->rax, remainder->rdx)
     break;
   }
-  push("   push rax\n");
+  printf("  push rax\n");
 
 }
+
+
 int main(int argc, char ** argv){
   if(argc != 2){
     fprintf(stderr, "Invalid number of arguments\n");
@@ -226,29 +233,19 @@ int main(int argc, char ** argv){
   }
 
   user_input = argv[1];
-  token = tokenize();
+  token = tokenize(user_input);
+  Node *node = expr();
 
+  // Print heads of code
   printf(".intel_syntax noprefix\n");
   printf(".global main \n");
   printf("main:\n");
 
-  ////// workspace
-  // First char must be number. (e.g. +1+20 is not formula)  
-  printf("  mov rax, %d\n", expect_number());
-  
-  while(!at_eof()){
-    if(consume('+')){
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
+  // generate code
+  gen(node);
 
-    // if it's not '+', it must be '-'.
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
-
-  //end
+  // stuck-top is result of calculation
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
